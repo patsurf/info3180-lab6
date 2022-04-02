c064e525f3b75c2c1eaa0a09b29e76ce6299e948
patricia surf <74924633+patsurf@users.noreply.github.com>
2022-04-02 11:35:56 -0500

    updated NewsList.vue

---
+++ src/components/NewsList.vue
@@ -0,0 +1,24 @@
+<template>
+ <ul class="news__list">
+ <li class="news__item">News item 1</li>
+ <li class="news__item">News item 2</li>
+ <li class="news__item">News item 3</li>
+ </ul>
+</template>
+<script>
+export default {
+ data() {
+ return {};
+ },
+};
+<script>
+import NewsList from '@/components/NewsList.vue'
+export default {
+ data() {
+ return {
+ message: "Hello World! Welcome to VueJS"
+ };
+ },
+ components: { NewsList }
+}
+</script>
